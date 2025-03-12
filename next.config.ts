1dd70f5d8bc60fc15e60960743a3378b0fb22d97
jack <phanoudeth@kokkok.la>
2025-03-12 15:42:53 +0700

    new deploy work flow

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 /**
  * @type {import('next').NextConfig}
  */
+const IsProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
+  basePath: IsProd ? '/my-profile' : '',
   output: 'export',
   images: {
     unoptimized: true,
